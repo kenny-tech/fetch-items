b79304e7fb557589e9cb5582e69c9d8567c86f89
kenny-tech <kenny4real2001@gmail.com>
2018-09-06 08:05:52 +0100

    Created the item list

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ItemList from '../src/components/ItemList';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ItemList />, document.getElementById('root'));
 registerServiceWorker();
